77dcc75e83681022b7f4169e8c5159ac3053defa
aerotecnia99 <valentinacamph@uc.cl>
2023-02-06 02:08:33 -0300

    product information almost ready

--- README.md
+++ README.md
@@ -1,70 +1,37 @@
-# Getting Started with Create React App
+# FinishedProductForm 
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Consideraciones generales
+En este repositorio se encuentra el código del formulario de Producto Terminado para ser rellenado y de la generación del PDF.
 
-## Available Scripts
 
-In the project directory, you can run:
+## Ejecución
+Corriendo el comando `npm start`
 
-### `npm start`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Navegación
+* `localhost:3000`: En esta página hay un botón que dice `Download`, el cual genera la plantilla del PDF del formulario de producto terminado.
+* `localhost:3000/finished-product`: En esta página se encuentra el formulario para ser rellenado por el operador. Este no se encuentra en su estado final, hay que hacerle varios cambios.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Módulos
 
-### `npm test`
+Los módulos que no aparecen en el siguiente listado, no son relevantes para el entendimiento del código. 
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+* ```src/components/ConvertToPdf.js```: En este módulo se encuentran todas las funciones relacionadas a la generación del PDF de producto terminado. El siguiente paso sería adaptarlo para recibir la información de un JSON y mostrarla en los campos que correspondan.
 
-### `npm run build`
+* ```src/components/LandingPage```: Es la página principal y en ella se encuentra el botón `Download` que llama a la función generadora del PDF.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+* ```src/components/FinishedProduct.js```: Es la página que contiene al formulario a ser rellenado por el operador. 
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+    Funciones:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+Dentro de la carpeta `finished_product`, se encuentran módulos que se utilizan dentro de `FinishedProduct.js`.
 
-### `npm run eject`
+* `GeneralInfo.js`:
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+* `ProductInformationTab.js`:
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+* `AdditionalSupplyInfoTab.js`:
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+## Consideraciones Adicionales
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/components/finished_product/ProductInformationTab.js
+++ src/components/finished_product/ProductInformationTab.js
@@ -1,3 +1,4 @@
+import Autocomplete from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
@@ -7,6 +8,7 @@ import { useState } from 'react';
 import { IconButton, Tooltip } from '@mui/material';
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 
+
 const product_fields = {
     time: '',
     pallet: '',
@@ -15,12 +17,18 @@ const product_fields = {
     pack_code: '',
     corrugated_code: '',
     sanitary_registration: '',
-    destiny: '',
+    destination: '',
     imported_by: '',
-    destiny_box: '',
+    destination_box: '',
     date: '',
     time_box: '',
     box_number: '',
+    destination_legend: '',
+    product_appearance: '',
+    informed_person: '',
+    paper_tearing: '',
+    inspection_status: '',
+    status_motif: ''
 }
 
 const responsive = {
@@ -44,6 +52,7 @@ function ProductInformationTab(props){
         [event.target.name]: event.target.value,
         }
         ));
+        console.log(data);
     };
 
     return (
@@ -82,10 +91,10 @@ function ProductInformationTab(props){
                 </Stack>
                 <Stack>
                     <Typography variant='body1' align='left'>Destino</Typography>
-                    <TextField value={data.destiny} name={"destiny"} onChange={updateData}></TextField>
+                    <TextField value={data.destination} name={"destiny"} onChange={updateData}></TextField>
                 </Stack>
             </Box>
-            <Paper sx={{mt: 3, pb: 1, pr: 1, pl: 1}} elevation={3}>
+            <Paper sx={{mt: 3, pb: 1, pr: 1, pl: 1, mb: 3}} elevation={3}>
                 <Typography variant='h6' align='left' sx={{ mb: 1, mt: 1 }}>Impresión en la Caja</Typography>
                 <Box sx={{ 
                     display: 'grid', 
@@ -105,10 +114,10 @@ function ProductInformationTab(props){
                     </Stack>
                     <Stack>
                         <Typography variant='body1' align='left'>Destino</Typography>
-                        <TextField value={data.destiny_box}  name={"destiny_box"} onChange={updateData}></TextField>
+                        <TextField value={data.destination_box}  name={"destiny_box"} onChange={updateData}></TextField>
                     </Stack>
                     <Stack>
-                        <Typography variant='body1' align='left'>Número de Cajas Inspeccionadas</Typography>
+                        <Typography variant='body1' align='left'>Nro. de las Cajas Inspeccionadas</Typography>
                         <TextField value={data.box_number}  name={"box_number"} onChange={updateData}></TextField>
                     </Stack>
                     <Stack>
@@ -121,6 +130,60 @@ function ProductInformationTab(props){
                     </Stack>
                 </Box>
             </Paper>
+            <Box sx={{ 
+                display: 'grid', 
+                gridTemplateColumns: responsive, 
+                gap: 3}}>
+                <Stack>
+                    <Typography variant='body1' align='left'>Leyenda del Destino</Typography>
+                    <Autocomplete
+                        disablePortal
+                        options={["OK", "NO OK", "N/A"]}
+                        name={"destination_legend"}
+                        renderInput={(params) => <TextField {...params}/>}
+                    />
+                </Stack>
+                <Stack>
+                    <Typography variant='body1' align='left'>Apariencia del Producto</Typography>
+                    <Autocomplete
+                        disablePortal
+                        options={["Buena", "Regular+", "Regular", "Regular-", "Mala"]}
+                        name={"product_appearance"}
+                        renderInput={(params) => <TextField {...params}/>}
+                    />
+                </Stack>
+                <Stack>
+                    <Typography variant='body1' align='left'>Persona Informada</Typography>
+                    <Autocomplete
+                        disablePortal
+                        options={["Nombre 1", "Nombre 2", "Nombre 3"]}
+                        name={"informed_person"}
+                        renderInput={(params) => <TextField {...params}/>}
+                    />
+                </Stack>
+                <Stack>
+                    <Typography variant='body1' align='left'>Desgarre de Papel</Typography>
+                    <Autocomplete
+                        disablePortal
+                        options={["Bueno", "Malo"]}
+                        name={"paper_tearing"}
+                        renderInput={(params) => <TextField {...params}/>}
+                    />
+                </Stack>
+                <Stack>
+                    <Typography variant='body1' align='left'>Estado de Inspección</Typography>
+                    <Autocomplete
+                        disablePortal
+                        options={["Aprobado", "En Proceso", "Detenido"]}
+                        name={"inspection_status"}
+                        renderInput={(params) => <TextField {...params}/>}
+                    />
+                </Stack>
+                <Stack>
+                    <Typography variant='body1' align='left'>Motivo del Estado de Inspección</Typography>
+                    <TextField value={data.status_motif}  name={"box_number"} onChange={updateData}></TextField>
+                </Stack>
+            </Box>
         </Box>
     )
 }
